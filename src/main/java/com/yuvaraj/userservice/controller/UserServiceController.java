5365e7dbdc2b56c5433dc6bdadd1b11d7ded39ce
yuvarajengineer@gmail.com <yuvarajengineer@gmail.com>
2025-01-19 20:58:09 +0530

    javac error

--- src/main/java/com/yuvaraj/userservice/controller/UserServiceController.java
+++ src/main/java/com/yuvaraj/userservice/controller/UserServiceController.java
@@ -1,5 +1,9 @@
 package com.yuvaraj.userservice.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
