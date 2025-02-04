35cd17abfbedadb30b6aba1132cf75ff84c817fa
yuvarajengineer <yuvarajengineer@gmail.com>
2025-02-04 19:35:04 +0530

    add time

--- src/main/java/com/yuvaraj/userservice/controller/UserController.java
+++ src/main/java/com/yuvaraj/userservice/controller/UserController.java
@@ -3,6 +3,8 @@ package com.yuvaraj.userservice.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/users")
@@ -11,4 +13,22 @@ public class UserController {
     public String getUsers() {
         return "User Service - List of Users";
     }
+
+    @GetMapping("/time/{number}")
+    public String getUserOrders(@PathVariable int number) {
+        System.out.println("Waiting for " + number + " seconds...");
+        try {
+            TimeUnit.SECONDS.sleep(number); // Sleeps for 10 minutes
+        } catch (InterruptedException e) {
+            System.out.println("Sleep interrupted: " + e.getMessage());
+        }
+        System.out.println("Done waiting!");
+        return "User Service - Time - " + number;
+    }
+
+    @GetMapping("/cpu")
+    public String getUserProfile() {
+        return "User Service - CPU";
+    }
+
 }
